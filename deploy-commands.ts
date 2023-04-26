f8bed69cc807ea23f532cb06981f196b7d30f839
Adrien N. F <iamnautilusjungle@gmail.com>
2023-04-25 22:30:31 -0400

    update types for deploy commands

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,48 +1,58 @@
-const { REST, Routes } = require('discord.js');
-const fs = require('node:fs');
-const path = require('node:path');
+
+import { REST, RESTPostAPIChatInputApplicationCommandsJSONBody, Routes } from 'discord.js';
+import fs from "fs";
+import path from "path";
 import dotenv from "dotenv";
+import { AppCommand } from './src/interfaces/AppCommand';
 
 dotenv.config();
 
-const commands: any[] = [];
-// Grab all the command files from the commands directory you created earlier
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const getCommands = async () => {
+	const foldersPath = path.join(process.env.ROOT_DIR as string, '/commands');
+	const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandFolders) {
-	// Grab all the command files from the commands directory you created earlier
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith('.ts'));
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
-		} else {
-			console.warn(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	const commands: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
+
+	for (const folder of commandFolders) {
+		const commandsPath = path.join(foldersPath, folder);
+		const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith('.ts'));
+
+		for (const file of commandFiles) {
+			const filePath = path.join(commandsPath, file);
+
+			await import(filePath).then((command: AppCommand) => {
+				if ('data' in command && 'execute' in command) {
+					commands.push(command.data.toJSON());
+				} else {
+					console.warn(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+				}
+			})
 		}
 	}
+
+	return commands;
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+const rest = new REST().setToken(process.env.DISCORD_TOKEN as string);
 
-// and deploy your commands!
-(async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+getCommands().then(commands => {
+	// and deploy your commands!
+	(async () => {
+		try {
+			console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationGuildCommands(process.env.DISCORD_APP_CLIENT_ID, process.env.DISCORD_SERVER_GUILD_ID),
-			{ body: commands },
-		);
+			// The put method is used to fully refresh all commands in the guild with the current set
+			const data = await rest.put(
+				Routes.applicationGuildCommands(process.env.DISCORD_APP_CLIENT_ID as string, process.env.DISCORD_SERVER_GUILD_ID as string),
+				{ body: commands },
+			) as unknown[];
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
-	}
-})();
+			console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		} catch (error) {
+			// And of course, make sure you catch and log any errors!
+			console.error(error);
+		}
+	})();
+})
+
